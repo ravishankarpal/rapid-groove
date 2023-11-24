5fa11b2838c1a68ab1de0dd0358dd1c6f9456685
Ravi Shankar <ravishankarpl3@gmail.com>
2023-11-24 17:10:59 +0530

    started project

---
+++ rapid-web/src/main/java/com/rapid/web/RapidGrooveOperations.java
@@ -0,0 +1,16 @@
+package com.rapid.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
+
+public class RapidGrooveOperations extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(RapidGrooveOperations.class, args);
+        System.out.println("Hello World!");
+    }
+}
