6168dd9c636c57e1f3b957fd417ab855e40ed042
Ravi Shankar <ravishankarpl3@gmail.com>
2023-12-07 22:28:27 +0530

    JWT Auth basic

--- rapid-web/src/main/java/com/rapid/web/RapidGrooveOperations.java
+++ rapid-web/src/main/java/com/rapid/web/RapidGrooveOperations.java
@@ -1,16 +1,31 @@
 package com.rapid.web;
 
+import com.rapid.dao.RoleRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
+@SpringBootApplication
+@Configuration
+@EnableJpaRepositories("com.rapid.dao")
+@EntityScan("com.rapid.core")
+@ComponentScan(basePackages  = {"com.rapid.service"})
 
 
-public class RapidGrooveOperations extends SpringBootServletInitializer {
+
+ public class RapidGrooveOperations extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(RapidGrooveOperations.class, args);
         System.out.println("Hello World!");
     }
+
+
 }
