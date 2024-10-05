25d03a309a1c2909cb30f33d1f1124ee9863ea09
Ravi Shankar <ravishankarpl3@gmail.com>
2024-10-05 22:08:39 +0530

    product fix impl

--- rapid-core/src/main/java/com/rapid/core/entity/product/Products.java
+++ rapid-core/src/main/java/com/rapid/core/entity/product/Products.java
@@ -1,10 +1,12 @@
 package com.rapid.core.entity.product;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -43,6 +45,36 @@ public class Products {
                     @JoinColumn(name = "image_id")
             })
     private Set<ImageModel> productImages;
+
+    @Column(name = "product_rating")
+    private Integer productRating;
+
+    @Column(name = "ratings_count")
+    private Integer ratingsCount;
+
+    @Column(name = "reviews_count")
+    private Integer reviewsCount;
+
+    @Column(name = "discount_percentage")
+    private Integer discountPercentage;
+
+    @Column(name = "available_sizes")
+    private String availableSizes;
+
+    @Column(name = "is_promotion_req")
+    private boolean isPromotionRequired;
+
+    @Column(name = "promotions")
+    private String promotions;
+
+    @Column(name = "is_tax_included")
+    private boolean isTaxIncluded;
+
+    @Column(name = "size")
+    private String size;
+
+
+
 
 
 }
